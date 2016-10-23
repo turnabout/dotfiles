3f7a60e77d3a327b2c231f8d645a103b2f154c4c
Kevin Pageau <turnabout.kevin@gmail.com>
2016-10-22 22:55:08 -0400

    Reorganize config files

--- config/initconfig.sh
+++ config/initconfig.sh
@@ -14,11 +14,6 @@ cat packages.list | xargs sudo apt-get -y install
 # Install gnome terminal color scheme (from https://github.com/Mayccoll/Gogh/blob/master/content/themes.md#monokai-dark)
 wget -O xt http://git.io/v3DBO && chmod +x xt && ./xt && rm xt
 
-# Install ST3
-sudo add-apt-repository ppa:webupd8team/sublime-text-3
-sudo apt-get update
-sudo apt-get install sublime-text-installer
-
 # Set Additional Workspaces
 gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ vsize 3
 gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ hsize 3
---
+++ config/sublime.sh
@@ -0,0 +1,8 @@
+################################
+# sublime.sh
+# Install Sublime Text 3
+################################
+
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+sudo apt-get update
+sudo apt-get install sublime-text-installer
