946f3e79b27a9b2fafbe1bc4a53648e1e043420b
Kevin Pageau <turnabout.kevin@gmail.com>
2016-10-23 15:56:10 -0400

    Update sublime install script

--- config/sublime.sh
+++ config/sublime.sh
@@ -3,14 +3,16 @@
 # Install Sublime Text 3
 ################################
 
+# Install
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
 sudo apt-get update
 sudo apt-get install sublime-text-installer
 
+# Open it once to generate config files
+subl
 
-st3packagesdir="/home/${USER}/.config/sublime-text-3/Packages/"
-
-cd "${st3packagesdir}"
+# Clone config files git repo
+cd "/home/${USER}/.config/sublime-text-3/Packages/"
 rm -r -f User
 
 git clone https://github.com/turnabout/sublime.git User
