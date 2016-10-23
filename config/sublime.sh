1668001bd93db7cb501c9a7e14425fe1bac4502c
Kevin Pageau <turnabout.kevin@gmail.com>
2016-10-23 15:47:49 -0400

    Add sublime user config cloning

--- config/sublime.sh
+++ config/sublime.sh
@@ -6,3 +6,13 @@
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
 sudo apt-get update
 sudo apt-get install sublime-text-installer
+
+
+st3packagesdir="/home/${USER}/.config/sublime-text-3/Packages/"
+
+cd "${st3packagesdir}"
+rm -r -f User
+
+git clone https://github.com/turnabout/sublime.git User
+
+cd ~
