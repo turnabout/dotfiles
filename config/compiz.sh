23ed4f6f117e815f4058b5b93100d41a193eefb4
Kevin Pageau <turnabout.kevin@gmail.com>
2016-10-23 15:39:45 -0400

    Add compiz profile only

--- config/compiz.sh
+++
@@ -1,7 +0,0 @@
-################################
-# compiz.sh
-# Import Compiz Config preferences
-################################
-
-dir=~/.dotfiles
-dconf load /org/compiz/profiles/mate/ < ${dir}/config/compiz-config.ini
