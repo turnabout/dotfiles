1cda07c0e6ff76bb86d0d6a99b8ca0a9d87121e3
Kevin Pageau <turnabout.kevin@gmail.com>
2016-10-24 04:55:01 -0400

    Change st3 script to manually create config dirs

--- config/sublime.sh
+++ config/sublime.sh
@@ -13,10 +13,15 @@ sudo apt-get update
 sudo apt-get install sublime-text-installer
 
 # Open it once to generate config files
-subl
+# subl
+
+# Manually create config directories
+cd "/home/${USER}/.config"
+
+mkdir -p sublime-text-3/Packages/User
+cd sublime-text-3/Packages
 
 # Clone config files git repo
-cd "${st3packagesfolder}"
 rm -r -f User
 
 git clone https://github.com/turnabout/sublime.git User
