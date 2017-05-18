c275426d180f0c7b745e442118ee6d6b1b154fdb
Kevin Pageau <kevin.pageau@momentumtravel.com>
2017-05-18 09:16:44 -0400

    Vim: Add NERD Commenter to plugins install script

--- .vim/bundle/setup.sh
+++ .vim/bundle/setup.sh
@@ -2,3 +2,4 @@ git clone https://github.com/jiangmiao/auto-pairs.git
 git clone https://github.com/airblade/vim-gitgutter.git
 git clone https://github.com/xolox/vim-misc.git
 git clone https://github.com/xolox/vim-notes.git
+git clone https://github.com/scrooloose/nerdcommenter.git
