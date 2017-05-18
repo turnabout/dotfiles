792f3e0ef4e34298586670444961d45376632b6e
Kevin Pageau <kevin.pageau@momentumtravel.com>
2017-05-18 10:58:30 -0400

    Vim: Add quick-scope plugin

--- .vim/bundle/setup.sh
+++ .vim/bundle/setup.sh
@@ -3,3 +3,4 @@ git clone https://github.com/airblade/vim-gitgutter.git
 git clone https://github.com/xolox/vim-misc.git
 git clone https://github.com/xolox/vim-notes.git
 git clone https://github.com/scrooloose/nerdcommenter.git
+git clone https://github.com/unblevable/quick-scope.git
