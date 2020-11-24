653cfbf764295001a63985eeb71eaa43f5c03bef
Kevin Pageau <pageau.kevin@gmail.com>
2020-11-24 17:11:47 -0500

    Update toggleMute to work with default sink

--- bin/toggleMute.sh
+++ bin/toggleMute.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-muted=`pacmd list-sinks | awk '/muted/ { print $2 }'`
+muted=`pacmd list-sinks|grep -A 15 '* index'|awk '/muted:/{ print $2 }'`
 
 if [ $muted == "no" ]; then
-    pactl set-sink-mute 0 1
+    pactl set-sink-mute @DEFAULT_SINK@ 1
 else
-    pactl set-sink-mute 0 0
+    pactl set-sink-mute @DEFAULT_SINK@ 0
 fi
 
 ~/.dotfiles/bin/sendGoBarSignal.sh SIGRTMIN+5
