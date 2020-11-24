2cf5dd6f9f156e73f5e97ef61b8fb2c19174988a
Kevin Pageau <pageau.kevin@gmail.com>
2020-11-24 17:38:57 -0500

    Adjust volume update scripts

--- bin/updateVolume.sh
+++ bin/updateVolume.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
-amixer -q sset Master $1
+
+SINK=$(pacmd list-sinks|awk '/\* index:/{ print $3 }') || "@DEFAULT_SINK@"
+
+# Check if volume is an increase
+if [[ "$1" == *"+"* ]]; then
+    # Get new volume after increase. If higher than max - exit early
+    VOL=$(pacmd list-sinks|grep -A 15 '* index'| awk '/volume: front/{ print $5 }' | sed 's/[%|,]//g')
+    INC=$(echo $1 | tr -dc '0-9')
+    TOTAL=$((VOL + INC))
+
+    if (( $TOTAL > 100 )); then
+        # TODO: Play different sound effect or something
+        exit
+    fi
+fi
+
+pactl set-sink-volume "$SINK" $1
 /home/kevin/.dotfiles/bin/sendGoBarSignal.sh SIGRTMIN+5
 aplay -q /home/kevin/bin/ding.wav
