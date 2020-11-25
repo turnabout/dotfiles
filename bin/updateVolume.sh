e72ba05783d13b02bb31d961bd2d769ddf10cc21
Kevin Pageau <pageau.kevin@gmail.com>
2020-11-25 09:06:40 -0500

    Clean up updateVolume.sh

--- bin/updateVolume.sh
+++ bin/updateVolume.sh
@@ -5,7 +5,7 @@ SINK=$(pacmd list-sinks|awk '/\* index:/{ print $3 }') || "@DEFAULT_SINK@"
 # Check if volume is an increase
 if [[ "$1" == *"+"* ]]; then
     # Get new volume after increase. If higher than max - exit early
-    VOL=$(pacmd list-sinks|grep -A 15 '* index'| awk '/volume: front/{ print $5 }' | sed 's/[%|,]//g')
+    VOL=$(pacmd list-sinks | grep -A 15 '* index' | awk '/volume: front/{ print $5 }' | sed 's/[%|,]//g')
     INC=$(echo $1 | tr -dc '0-9')
     TOTAL=$((VOL + INC))
 
